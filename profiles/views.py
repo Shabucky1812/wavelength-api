ea09360399c27244be670c62dd0ed2ebef21379c
Shabucky1812 <shaunbuck1812@gmail.com>
2023-08-15 13:58:10 +0000

    Create put method for Profile Detail view

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
 from django.http import Http404
+from rest_framework import status
 
 
 class ProfileList(APIView):
@@ -13,6 +14,8 @@ class ProfileList(APIView):
 
 
 class ProfileDetail(APIView):
+    serializer_class = ProfileSerializer
+
     def get_object(self, id):
         try:
             profile = Profile.objects.get(id=id)
@@ -24,3 +27,11 @@ class ProfileDetail(APIView):
         profile = self.get_object(id)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+    def put(self, request, id):
+        profile = self.get_object(id)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
