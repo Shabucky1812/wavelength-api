555504b2f9fd46e7e94cf89e0caf72e6d5db580c
Shabucky1812 <shaunbuck1812@gmail.com>
2023-08-16 13:45:32 +0000

    Create TrackList get view

--- tracks/views.py
+++ tracks/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Track
+from .serializers import TrackSerializer
 
-# Create your views here.
+
+class TrackList(APIView):
+    def get(self, request):
+        tracks = Track.objects.all()
+        serializer = TrackSerializer(
+            tracks, many=True, context={'request': request})
+        return Response(serializer.data)
