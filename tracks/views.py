66428bf7e8623fdbbfbd62604e928f9f5e5971a6
Shabucky1812 <shaunbuck1812@gmail.com>
2023-08-16 14:05:42 +0000

    Create TrackList post func

--- tracks/views.py
+++ tracks/views.py
@@ -1,12 +1,31 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status, permissions
 from .models import Track
 from .serializers import TrackSerializer
 
 
 class TrackList(APIView):
+    serializer_class = TrackSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+
     def get(self, request):
         tracks = Track.objects.all()
         serializer = TrackSerializer(
             tracks, many=True, context={'request': request})
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TrackSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
