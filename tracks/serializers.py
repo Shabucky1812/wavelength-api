aef4061dbf2e95c0b1fa9e71b82a2e6eb205b44e
Shabucky1812 <shaunbuck1812@gmail.com>
2023-08-17 12:44:11 +0000

    Add average_score field to TrackSerializer

--- tracks/serializers.py
+++ tracks/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from tracks.models import Track
+from reviews.models import Review
 
 
 class TrackSerializer(serializers.ModelSerializer):
@@ -7,14 +8,25 @@ class TrackSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    average_score = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_average_score(self, obj):
+        track_reviews = Review.objects.filter(track=obj.id)
+        if track_reviews:
+            scores = []
+            for review in track_reviews:
+                scores.append(review.score)
+            return round(sum(scores) / len(scores))
+        return None
+
     class Meta:
         model = Track
         fields = [
             'id', 'owner', 'track_ref', 'title', 'artist', 'cover_art',
-            'genre', 'opinion', 'is_owner', 'profile_id', 'profile_image'
+            'genre', 'opinion', 'is_owner', 'profile_id', 'profile_image',
+            'average_score'
         ]
