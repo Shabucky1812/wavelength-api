4b537cc884d5075f32c52333dc70bea4f6b5c92a
Shabucky1812 <shaunbuck1812@gmail.com>
2023-08-19 08:44:01 +0000

    refactor Track views to generics

--- tracks/urls.py
+++ tracks/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('tracks/', views.TrackList.as_view()),
-    path('tracks/<int:id>/', views.TrackDetail.as_view()),
+    path('tracks/<int:pk>/', views.TrackDetail.as_view()),
 ]
--- tracks/views.py
+++ tracks/views.py
@@ -1,67 +1,19 @@
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import status, permissions
-from django.http import Http404
+from rest_framework import generics, permissions
 from .models import Track
 from .serializers import TrackSerializer
 from wavelength.permissions import IsOwnerOrReadOnly
 
 
-class TrackList(APIView):
+class TrackList(generics.ListCreateAPIView):
     serializer_class = TrackSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Track.objects.all()
 
-    def get(self, request):
-        tracks = Track.objects.all()
-        serializer = TrackSerializer(
-            tracks, many=True, context={'request': request})
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = TrackSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class TrackDetail(APIView):
+class TrackDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TrackSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, id):
-        try:
-            track = Track.objects.get(id=id)
-            self.check_object_permissions(self.request, track)
-            return track
-        except Track.DoesNotExist:
-            raise Http404
-
-    def get(self, request, id):
-        track = self.get_object(id)
-        serializer = TrackSerializer(track, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, id):
-        track = self.get_object(id)
-        serializer = TrackSerializer(
-            track, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, id):
-        track = self.get_object(id)
-        track.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Track.objects.all()
